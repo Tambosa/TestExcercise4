e3db678baba1b9b8c642d57c1fcc8edac3cc2e5b
Tambosa <tambosa@gmail.com>
2022-10-22 15:44:25 +0300

    поведение bottom navigation

--- app/src/main/java/com/aroman/testexcercise4/ui/MainActivity.kt
+++ app/src/main/java/com/aroman/testexcercise4/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aroman.testexcercise4.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.aroman.testexcercise4.R
 import com.aroman.testexcercise4.databinding.ActivityMainBinding
 import com.aroman.testexcercise4.ui.classesFragment.ClassesFragment
 import com.aroman.testexcercise4.ui.homeFragment.HomeFragment
@@ -17,10 +18,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fragmentAdapter.addFragment(HomeFragment())
-        fragmentAdapter.addFragment(ClassesFragment())
+        initBottomNavigation()
+        initViewPager()
+    }
 
-        binding.pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        binding.pager.adapter = fragmentAdapter
+    private fun initBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_home -> binding.pager.currentItem = 0
+                R.id.action_classes -> binding.pager.currentItem = 1
+            }
+            true
+        }
+    }
+
+    private fun initViewPager() {
+        fragmentAdapter.apply {
+            addFragment(HomeFragment())
+            addFragment(ClassesFragment())
+        }
+        binding.pager.apply {
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            adapter = fragmentAdapter
+        }
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when (position) {
+                    0 -> {
+                        binding.bottomNavigation.menu.getItem(0).isChecked = true
+                        binding.bottomNavigation.menu.getItem(1).isChecked = false
+                    }
+                    1 -> {
+                        binding.bottomNavigation.menu.getItem(0).isChecked = false
+                        binding.bottomNavigation.menu.getItem(1).isChecked = true
+                    }
+                }
+            }
+        })
     }
 }
