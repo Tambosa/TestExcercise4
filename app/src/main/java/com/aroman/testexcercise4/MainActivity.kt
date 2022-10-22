14f32082a4f3ff44153e7e57ca23b625eb802cea
Tambosa <tambosa@gmail.com>
2022-10-22 13:00:57 +0300

    Initial commit

---
+++ app/src/main/java/com/aroman/testexcercise4/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.aroman.testexcercise4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aroman.testexcercise4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
